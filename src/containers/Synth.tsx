3798e4c8c8153eca934ec5da496400beb3140dd9
Daniel Le'Sage <DLesage25@users.noreply.github.com>
2020-09-25 19:30:33 -0600

    Heroku

--- src/containers/Synth.tsx
+++ src/containers/Synth.tsx
@@ -8,7 +8,7 @@ import ControlPanel from 'components/synth/ControlPanel';
 import Keyboard from 'components/synth/Keyboard';
 import Oscilloscope from 'components/synth/Oscilloscope';
 
-import { useKeysPress } from '../hooks';
+// import { useKeysPress } from '../hooks';
 import { SynthState } from 'store/types';
 
 const soundTypes = ['triangle', 'square', 'sine', 'sawtooth'];
@@ -18,37 +18,38 @@ const Synth = ({ synthEngine }: { synthEngine: any }) => {
     const { octave, duration } = useSelector(({ synth }: SynthState) => synth);
 
     // const largeSceen = useMedia('(min-width: 800px)');
-    const [key] = useKeysPress();
+    // const [key] = useKeysPress();
+    const key = '0';
 
     // control octave and filters
-    useEffect(() => {
-        if ('1234'.search(key) >= 0) {
-            dispatch({
-                type: 'SYNTH_TYPE',
-                payload: soundTypes['1234'.search(key)],
-            });
-        } else if ('-z'.search(key) >= 0) {
-            dispatch({
-                type: 'SYNTH_OCTAVE',
-                payload: Math.max(octave - 1, 0),
-            });
-        } else if ('=x'.search(key) >= 0) {
-            dispatch({
-                type: 'SYNTH_OCTAVE',
-                payload: Math.min(octave + 1, 2),
-            });
-        } else if ('['.search(key) >= 0) {
-            dispatch({
-                type: 'SYNTH_DURATION',
-                payload: Math.max(duration - 0.5, 0.5),
-            });
-        } else if (']'.search(key) >= 0) {
-            dispatch({
-                type: 'SYNTH_DURATION',
-                payload: Math.min(duration + 0.5, 10),
-            });
-        }
-    }, [key]);
+    // useEffect(() => {
+    //     if ('1234'.search(key) >= 0) {
+    //         dispatch({
+    //             type: 'SYNTH_TYPE',
+    //             payload: soundTypes['1234'.search(key)],
+    //         });
+    //     } else if ('-z'.search(key) >= 0) {
+    //         dispatch({
+    //             type: 'SYNTH_OCTAVE',
+    //             payload: Math.max(octave - 1, 0),
+    //         });
+    //     } else if ('=x'.search(key) >= 0) {
+    //         dispatch({
+    //             type: 'SYNTH_OCTAVE',
+    //             payload: Math.min(octave + 1, 2),
+    //         });
+    //     } else if ('['.search(key) >= 0) {
+    //         dispatch({
+    //             type: 'SYNTH_DURATION',
+    //             payload: Math.max(duration - 0.5, 0.5),
+    //         });
+    //     } else if (']'.search(key) >= 0) {
+    //         dispatch({
+    //             type: 'SYNTH_DURATION',
+    //             payload: Math.min(duration + 0.5, 10),
+    //         });
+    //     }
+    // }, [key]);
 
     return (
         <Grid container alignContent="center" justify="center">
